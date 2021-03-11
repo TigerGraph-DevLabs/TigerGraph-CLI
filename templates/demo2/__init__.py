e3b36b5d4f4e56cbcce5b298413bf79dbb90b368
Zrouga Mohamed <zrouga.mohammed@gmail.com>
2021-03-11 17:29:13 +0100

    initial commit..

---
+++ templates/demo2/__init__.py
@@ -0,0 +1,12 @@
+
+
+
+def start():
+    pass
+
+def info():
+    return {
+        'id': "demo2",
+        'name': 'Fraud Kit ( Work in Progress ) ',
+        'tags': ['demo', 'Fraud Kit']
+    }
---
+++ templates/demo3/__init__.py
@@ -0,0 +1,10 @@
+
+def start():
+    pass
+
+def info():
+    return {
+        'id': "demo3",
+        'name': 'GSQL 101 (Work in Progress ) ',
+        'tags': ['demo', 'Fraud Kit']
+    }
